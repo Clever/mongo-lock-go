3e21118052e43098c08792778108f8f66e3e93e9
Fred Sun <fredtsun@gmail.com>
2024-01-23 16:34:26 -0800

    Update r unlock tests

--- rw_mutex_unlock_test.go
+++ rw_mutex_unlock_test.go
@@ -1,41 +1,42 @@
 package lock
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
-// 	"gopkg.in/mgo.v2/bson"
-// )
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-// // TestRUnlockSuccess - RWMutex.RUnlock releases the lock correctly
-// func TestRUnlockSuccess(t *testing.T) {
-// 	c := setupRWMutexTest(t)
-// 	lock := NewRWMutex(c, lockID, clientID)
-// 	require.NoError(t, lock.RLock())
+// TestRUnlockSuccess - RWMutex.RUnlock releases the lock correctly
+func TestRUnlockSuccess(t *testing.T) {
+	c := setupRWMutexTest(t)
+	lock := NewRWMutex(c.collection, lockID, clientID)
+	require.NoError(t, lock.RLock())
 
-// 	// add a second lock to check RWMutex only removes the read lock for its client
-// 	secondLock := NewRWMutex(c, lockID, "client_2")
-// 	require.NoError(t, secondLock.RLock())
+	// add a second lock to check RWMutex only removes the read lock for its client
+	secondLock := NewRWMutex(c.collection, lockID, "client_2")
+	require.NoError(t, secondLock.RLock())
 
-// 	err := lock.RUnlock()
-// 	assert.NoError(t, err)
-// 	var mLock mongoLock
-// 	require.NoError(t, c.Find(bson.M{
-// 		"lockID": lockID,
-// 	}).One(&mLock))
-// 	assert.Equal(t, mongoLock{
-// 		LockID:  lockID,
-// 		Readers: []string{"client_2"},
-// 		Writer:  "",
-// 	}, mLock)
-// }
+	err := lock.RUnlock()
+	assert.NoError(t, err)
+	var mLock mongoLock
+	c.FindOne(t, bson.M{
+		"lockID": lockID,
+	}, options.FindOne(), &mLock)
+	assert.Equal(t, mongoLock{
+		LockID:  lockID,
+		Readers: []string{"client_2"},
+		Writer:  "",
+	}, mLock)
+}
 
-// // TestRUnlockNotHeld - RWMutex.RUnlock returns an error if the client did not hold the lock
-// func TestRUnlockNotHeld(t *testing.T) {
-// 	c := setupRWMutexTest(t)
-// 	lock := NewRWMutex(c, lockID, clientID)
+// TestRUnlockNotHeld - RWMutex.RUnlock returns an error if the client did not hold the lock
+func TestRUnlockNotHeld(t *testing.T) {
+	c := setupRWMutexTest(t)
+	lock := NewRWMutex(c.collection, lockID, clientID)
 
-// 	err := lock.RUnlock()
-// 	assert.Error(t, err)
-// }
+	err := lock.RUnlock()
+	assert.Error(t, err)
+}
