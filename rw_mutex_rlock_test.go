5b6803c3ba1f15399b1a71ab294e02a601d3768d
Garrett Gordon <garrett.gordon@clever.com>
2024-04-17 12:26:35 -0700

    test fixes

--- rw_mutex_rlock_test.go
+++ rw_mutex_rlock_test.go
@@ -187,7 +187,7 @@ func TestRLockMultipleReaders(t *testing.T) {
 func TestRDistrictIDLockMultipleReaders(t *testing.T) {
 	c := setupRWMutexTest(t)
 
-	firstLock := NewRWMutex(c.collection, districtID, "client_2", districtID, false)
+	firstLock := NewRWMutex(c.collection, districtID, "client_2", districtID, true)
 	err := firstLock.RLock()
 	require.NoError(t, err)
 
@@ -205,9 +205,9 @@ func TestRDistrictIDLockMultipleReaders(t *testing.T) {
 	require.NoError(t, err)
 
 	var mLock mongoLock
-	c.FindOne(t, bson.M{"lockID": districtID}, options.FindOne(), &mLock)
+	c.FindOne(t, bson.M{"lockID": lockID}, options.FindOne(), &mLock)
 	assert.Equal(t, mongoLock{
-		LockID:  districtID,
+		LockID:  lockID,
 		Readers: []string{"client_2", clientID},
 	}, mLock)
 }
@@ -251,18 +251,18 @@ func TestRDistrictIDLockReenter(t *testing.T) {
 	// get the read lock for the first time
 	require.NoError(t, lock.RLock())
 	var mLock mongoLock
-	c.FindOne(t, bson.M{"lockID": districtID}, options.FindOne(), &mLock)
+	c.FindOne(t, bson.M{"lockID": lockID}, options.FindOne(), &mLock)
 	assert.Equal(t, mongoLock{
-		LockID:  districtID,
+		LockID:  lockID,
 		Writer:  "",
 		Readers: []string{clientID},
 	}, mLock)
 
 	// check that grabbing it again yields same results
 	require.NoError(t, lock.RLock())
-	c.FindOne(t, bson.M{"lockID": districtID}, options.FindOne(), &mLock)
+	c.FindOne(t, bson.M{"lockID": lockID}, options.FindOne(), &mLock)
 	assert.Equal(t, mongoLock{
-		LockID:  districtID,
+		LockID:  lockID,
 		Writer:  "",
 		Readers: []string{clientID},
 	}, mLock)
